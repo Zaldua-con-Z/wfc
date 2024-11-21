69fa9c2d1ff4351542a9fa05a59e88cf22e8c1d7
Zaldua-con-Z <camila.zaldua@ucuenca.edu.ec>
2024-11-20 22:53:24 -0500

    Monitoreo de azulejos

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const celdas = []; 
-const RETICULA = 8;
+const RETICULA = 6;
 let ancho; //anchura de la celda
 let alto; // altura de la celda
 
 const azulejos = [];
 const NA = 11; //numero de los azulejos
+
+let opcionesI = [];
 
 const reglas = [
   // reglas de los bordes de cada azulejo
@@ -100,7 +102,6 @@ function setup (){
     ancho = width/ RETICULA;
     alto = height / RETICULA;
 
-    let opcionesI = [];
     for (let i = 0 ; i <azulejos.length; i++){
       opcionesI.push(i);
     }
@@ -111,20 +112,22 @@ function setup (){
         opciones: opcionesI,
       };
     }
-    celdas[8].colapsada = true;
-    celdas[3].colapsada = true;
+    //celdas[8].colapsada = true;
+    //celdas[3].colapsada = true;
 
-    celdas[12].opciones = [ 5,6,8];
-    celdas[4].opciones = [4,7,12];
-    celdas [6].opciones [9,7,12]
-    celdas[1].opciones = [6,4,8,10];
-    celdas[5].opciones = [11,6,4,8,10];
+    //celdas[12].opciones = [ 5,6,8];
+    //celdas[4].opciones = [4,7,12];
+    //celdas [6].opciones [9,7,12]
+    //celdas[1].opciones = [6,4,8,10];
+    //celdas[5].opciones = [11,6,4,8,10];
   }
 
 function draw (){
-  //background(111)
+  //background(255,0,255);
+  
   const celdasDisponibles = celdas.filter((celda) =>{ return celda.colapsada == false
   });
+
   if(celdasDisponibles.length > 0 ){
     celdasDisponibles.sort((a,b)=> { return a.opciones.length - b.opciones.length;
     });
@@ -140,25 +143,78 @@ function draw (){
     celdaSeleccionada.opciones = [opcionSeleccionada]
 
 
-    print(celdaSeleccionada);
+    //print(celdaSeleccionada);
 
-    
     for (let x = 0; x < RETICULA; x++) {
       for(let y = 0; y < RETICULA; y++) {
         const celdaIndex = x + y * RETICULA;
         const celdaActual= celdas [celdaIndex];
         if(celdaActual.colapsada){
+          const indiceDeAzulejo = celdaActual.opciones [0];
+          const reglasActuales = reglas[indiceDeAzulejo];
+          //print(reglasActuales);
           image(
-            azulejos[celdaActual.opciones[0]], 
+            azulejos[indiceDeAzulejo], 
             x*ancho,
             y*alto,
             ancho,
             alto
            );
+           //Monitorear UP
+           if(y > 0){
+            const indiceUP = x + (y - 1) * RETICULA;
+            const celdaUP = celdas [indiceUP];
+            if(!celdaUP.colapsada){
+              cambiarEntropia(celdaUP, reglasActuales['UP'],'DOWN');
+            }
+           }
+           //Monitorear RIGHT
+           if(x < RETICULA - 1){
+            const indiceRIGHT = x + 1 + y * RETICULA;
+            const celdaRIGHT = celdas [indiceRIGHT];
+            if(!celdaRIGHT.colapsada){
+              cambiarEntropia(celdaRIGHT, reglasActuales['RIGHT'],'LEFT');
+            }
+           }
+           //Monitorear DOWN
+           if(y < RETICULA - 1){
+            const indiceDOWN = x + (y + 1) * RETICULA;
+            const celdaDOWN = celdas [indiceDOWN];
+            if(!celdaDOWN.colapsada){
+              cambiarEntropia(celdaDOWN, reglasActuales['DOWN'],'UP');
+            }
+           }
+           //Monitorear LEFT
+           if(x > 0){
+            const indiceLEFT = x - 1 + y  * RETICULA;
+            const celdaLEFT = celdas [indiceLEFT];
+            if(!celdaLEFT.colapsada){
+              cambiarEntropia(celdaLEFT, reglasActuales['LEFT'],'RIGHT'); 
+            }
+           } 
         }
       }
     }
   
   //noLoop();
+  } //else{
+    //for (let i = 0; i < RETICULA * RETICULA; i++){ 
+      //celdas [i]={
+       // colapsada: false,
+       // opciones: opcionesI,
+      //};
+    //}
+  //}
+}
+
+function cambiarEntropia(_celda, _regla, _opuesta){
+  const nuevasOpciones = [];
+  for(let i = 0; i < _celda.opciones.length; i ++){
+    if(_regla == reglas[_celda.opciones[i]][_opuesta]) {
+      const celdaCompatible = _celda.opciones[i];
+      nuevasOpciones.push(celdaCompatible);
+    }
   }
+  _celda.opciones = nuevasOpciones;
+  print(nuevasOpciones);
 }
